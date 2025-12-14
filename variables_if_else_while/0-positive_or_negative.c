48178601200b8e3aba3d37c46c8efbee4913fb39
Izzat <shixbalayevizzet@example.com>
2025-12-14 18:48:35 +0000

    task0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -9,13 +9,13 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	printf ("%d is ", n);
+	printf("%d is ", n);
 	if (n > 0)
-		printf ("positive\n");
+		printf("positive\n");
 	else if (n == 0)
-		printf ("zero\n");
+		printf("zero\n");
 	else
-		printf ("negative\n");
+		printf("negative\n");
 
-	return(0);
+	return (0);
 }
