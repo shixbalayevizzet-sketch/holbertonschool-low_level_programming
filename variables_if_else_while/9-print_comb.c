17aaa5075c96f2e0703e01f9d0e70161cd516b2b
Izzat <shixbalayevizzet@example.com>
2025-12-15 21:23:27 +0000

    task9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -16,13 +16,9 @@ int main(void)
 			putchar(i + '0');
 			putchar(j + '0');
 			if (i != 8 || j != 9)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+				putchar(','); putchar(' ');
 		}
 	}
-
 	putchar('\n');
 
 	return (0);
