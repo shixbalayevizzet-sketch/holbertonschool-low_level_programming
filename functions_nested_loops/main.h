7f3533b173c5eaaa11c160429729e6465e4092c7
Izzat <shixbalayevizzet@example.com>
2025-12-16 14:15:07 +0000

    task1

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* _putchar funksiyasının prototipi */
 int _putchar(char c);
+
+/* print_alphabet funksiyasının prototipi */
+void print_alphabet(void);
 
 #endif
