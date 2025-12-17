d3f3c4e75ab4d41cc3a76b18838758db41273491
Izzat <shixbalayevizzet@example.com>
2025-12-17 13:18:52 +0000

    task6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - Farklı veri tiplerinin boyutlarını yazdırır
+ * main - Prints the size of various types on the computer
  *
- * Return: Her zaman 0 (Başarı)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(long long int));
+	printf("Size of a long long int: %lu byte(s)\n",
+	       (unsigned long)sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
 
 	return (0);
