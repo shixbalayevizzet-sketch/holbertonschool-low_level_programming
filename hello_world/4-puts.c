d41e4af602ab71b04dd151e784ffeb93dd70cb01
Izzat <shixbalayevizzet@example.com>
2025-12-17 13:02:41 +0000

    task4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Programın giriş noktası
+ *
+ * Return: Her zaman 0 (Başarı)
+ */
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
