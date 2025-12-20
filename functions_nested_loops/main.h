1243277bb3128d2b20215943b64152e485cc26c2
Izzat <Shixbalayevizzet@example.com>
2025-12-20 22:05:18 +0000

    task9

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -6,9 +6,9 @@ int _putchar(char c);
 int _islower(int c);
 /* print_alphabet funksiyasının prototi */
 void jack_bauer(void);
-
-
-
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _isalpha(int c);
