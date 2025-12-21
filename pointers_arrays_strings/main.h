22b8e90c2b9866b500d8eaf218ff90c8f87cf67e
Izzat <shixbalayevizzet@example.com>
2025-12-21 21:32:51 +0000

    task1

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -6,9 +6,9 @@ int _putchar(char c);
 void reset_to_98(int *n);
 
 
-
-
-
-
-
+void rev_string(char *s);
+void print_rev(char *s);
+void _puts(char *str);
+int _strlen(char *s);
+void swap_int(int *a, int *b);
 #endif
