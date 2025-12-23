0356c5cb6f441fad10f31c38f05de660711ff018
Izzat <shixbalayevizzet@example.com>
2025-12-23 13:38:13 +0000

    task2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: destination buffer
+ * @src: source string
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to the destination string dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+
+	return (dest);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -2,6 +2,11 @@
 #define MAIN_H
 char *_strcat(char *dest, char *src);
 int _putchar(char c);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
 void reset_to_98(int *n);
 void puts_half(char *str);
 void print_array(int *a, int n);
