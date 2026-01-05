cc232aa877b004c934bbdfe283c8971680d71ba1
Izzat <shixbalayevizzet@example.com>
2026-01-05 09:36:28 +0000

    Task0

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,5 +1,9 @@
 /**
  * _memset - fills memory with a constant byte
+ * @s: pointer to the memory area to be filled
+ * @b: the constant byte to fill the memory with
+ * @n: the number of bytes to be filled
+ *
  * Return: a pointer to the memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
