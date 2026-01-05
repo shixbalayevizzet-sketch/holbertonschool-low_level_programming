fd370ae0a633815b6892a681b1c6813b42990b2c
Izzat <shixbalayevizzet@example.com>
2026-01-05 16:45:19 +0000

    task0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    /* argv[0] contains the name of the program or the path used to run it */
+    printf("%s\n", argv[0]);
+    return 0;
+}
