75e6c9ef9018a802b9bac4f201eeafa88351c95d
Izzat <shixbalayevizzet@example.com>
2026-01-05 10:24:02 +0000

    task0

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -2,9 +2,7 @@
 
 /**
  * _puts_recursion - prints a string, followed by a new line
- * @s: the string to print
- *
- * Return: void
+ * @s: string to print
  */
 void _puts_recursion(char *s)
 {
@@ -13,7 +11,6 @@ void _puts_recursion(char *s)
 		_putchar('\n');
 		return;
 	}
-
 	_putchar(*s);
 	_puts_recursion(s + 1);
 }
