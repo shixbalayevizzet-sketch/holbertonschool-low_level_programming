7e7ddd299af03fb479f6c40022bd8a9790e98c3e
Izzat <shixbalayevizzet@example.com>
2026-01-05 09:44:33 +0000

    Task2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,24 +1,22 @@
+#include "main.h"
+#include <stddef.h>
+
 /**
  * _strchr - locates a character in a string
  * @s: the string to search
  * @c: the character to find
  *
- * Return: a pointer to the first occurrence of c, 
+ * Return: a pointer to the first occurrence of c,
  * or NULL if the character is not found
  */
 char *_strchr(char *s, char c)
 {
-	while (*s >= '\0')
+	while (1)
 	{
 		if (*s == c)
-		{
 			return (s);
-		}
 		if (*s == '\0')
-		{
-			return ('\0');
-		}
+			return (NULL);
 		s++;
 	}
-	return ('\0');
 }
