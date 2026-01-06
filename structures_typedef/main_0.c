8d0fe0c3ee4eacc86b2db1f1f367671d3880adf4
Izzat <shixbalayevizzet@example.com>
2026-01-06 07:37:12 +0000

    task1

--- structures_typedef/main_0.c
+++ structures_typedef/main_0.c
@@ -1,6 +1,20 @@
-#ifndef <stdio.h>
-#define "dog.h"
-void init_dog(struct dog *d, char *name, float age, char *owner);
+#include <stdio.h>
+#include "dog.h" /* Funksiya prototipini və struct-ı buradan götürür */
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	struct dog my_dog;
 
-#endif 
+	/* init_dog funksiyasını çağırırıq */
+	init_dog(&my_dog, "Ghost", 4.75, "Jon Snow");
+	
+	/* Nəticəni yoxlamaq üçün çap edirik */
+	printf("My name is %s, and I am %.2f :) - Woof!\n", my_dog.name, my_dog.age);
+	
+	return (0);
+}
