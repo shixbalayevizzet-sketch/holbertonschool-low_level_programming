19b260ed6426a1306c06cddc187d563ebfd03231
Izzat <shixbalayevizzet@example.com>
2026-01-07 08:20:55 +0000

    task0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
+#include "function_pointers.h"
+#include <stdlib.h>
 
 /**
- * print_name - prints a name using a specific function
+ * print_name - prints a name
  * @name: name of the person
  * @f: pointer to the printing function
- *
- * Return: nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
