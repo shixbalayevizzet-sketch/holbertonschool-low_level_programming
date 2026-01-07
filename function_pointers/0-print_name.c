1420fdb6df3531f194e43cab1581ce9c6c1827f3
Izzat <shixbalayevizzet@example.com>
2026-01-07 08:22:44 +0000

    task0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -5,11 +5,13 @@
  * print_name - prints a name
  * @name: name of the person
  * @f: pointer to the printing function
+ *
+ * Return: nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
-    if (name != NULL && f != NULL)
-    {
-        f(name);
-    }
+	if (name != NULL && f != NULL)
+	{
+		f(name);
+	}
 }
