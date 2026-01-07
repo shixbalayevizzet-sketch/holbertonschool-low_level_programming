83a7cf6ad1854f373d56ccb39bebaaaa393e5607
Izzat <shixbalayevizzet@example.com>
2026-01-07 10:56:18 +0000

    task3

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,45 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_all - Prints anything based on a format string
+ * @format: A list of types of arguments passed to the function
+ */
+void print_all(const char * const format, ...)
+{
+    va_list args;
+    unsigned int i = 0;
+    char *str, *sep = "";
+
+    va_start(args, format);
+
+    while (format && format[i])
+    {
+        switch (format[i])
+        {
+            case 'c':
+                printf("%s%c", sep, va_arg(args, int));
+                sep = ", ";
+                break;
+            case 'i':
+                printf("%s%d", sep, va_arg(args, int));
+                sep = ", ";
+                break;
+            case 'f':
+                printf("%s%f", sep, va_arg(args, double));
+                sep = ", ";
+                break;
+            case 's':
+                str = va_arg(args, char *);
+                if (!str)
+                    str = "(nil)";
+                printf("%s%s", sep, str);
+                sep = ", ";
+                break;
+        }
+        i++;
+    }
+
+    printf("\n");
+    va_end(args);
+}
