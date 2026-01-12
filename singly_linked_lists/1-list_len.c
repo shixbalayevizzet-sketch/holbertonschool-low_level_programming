460905364664890e36727ebfea04b1d545469d25
Izzat <shixbalayevizzet@gmail.com>
2026-01-12 18:14:37 +0000

    task1

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list_t list
+ * @h: pointer to the start of the linked list
+ *
+ * Return: the number of elements in h
+ */
+size_t list_len(const list_t *h)
+{
+	size_t nodes = 0;
+
+	while (h != NULL)
+	{
+		nodes++;
+		h = h->next; /* Move to the next node */
+	}
+
+	return (nodes);
+}
--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -20,5 +20,5 @@ typedef struct list_s
 
 /* Use a pointer (*) for the parameter h */
 size_t print_list(const list_t *h);
-
+size_t list_len(const list_t *h);
 #endif
