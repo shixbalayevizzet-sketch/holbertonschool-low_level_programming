331698fa2fb6289c2e46988c14413a70a48cd95a
Izzat <shixbalayevizzet@gmail.com>
2026-01-12 18:24:03 +0000

    task4

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to the list_t list to be freed
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next; 
+		free(head->str);   
+		free(head);       
+		head = temp;      
+	}
+}
