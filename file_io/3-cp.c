7b0c468587014b5bcff2cd60a856510bd923bb5d
Izzat <shixbalayevizzet@example.com>
2026-01-19 17:29:39 +0000

    task3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -4,6 +4,19 @@
 #include <unistd.h>
 
 /**
+ * close_fd - Closes a file descriptor and exits on failure.
+ * @fd: The file descriptor to close.
+ */
+void close_fd(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
  * main - Copies the content of a file to another file.
  * @argc: The number of arguments.
  * @argv: An array of pointers to the arguments.
@@ -21,23 +34,19 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
-	/* rw-rw-r-- icazəsi 0664-ə bərabərdir */
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		close(fd_from);
+		close_fd(fd_from);
 		exit(99);
 	}
-
 	while ((n_read = read(fd_from, buffer, 1024)) > 0)
 	{
 		n_written = write(fd_to, buffer, n_read);
@@ -47,24 +56,12 @@ int main(int argc, char *argv[])
 			exit(99);
 		}
 	}
-
 	if (n_read == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
-	if (close(fd_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
-
-	if (close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
-
+	close_fd(fd_from);
+	close_fd(fd_to);
 	return (0);
 }
