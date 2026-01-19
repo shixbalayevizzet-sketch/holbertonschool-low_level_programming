eaa2bdf12f2dbd31ecdc43952367b85aa6b3e6dd
Izzat <shixbalayevizzet@example.com>
2026-01-19 17:15:03 +0000

    task0

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,48 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - Reads a text file and prints it to POSIX standard output.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters to read and print.
+ * * Return: The actual number of letters read and printed, or 0 on failure.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+    int fd;
+    ssize_t bytes_read, bytes_written;
+    char *buffer;
+
+    if (filename == NULL)
+        return (0);
+
+    buffer = malloc(sizeof(char) * letters);
+    if (buffer == NULL)
+        return (0);
+
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+    {
+        free(buffer);
+        return (0);
+    }
+
+    bytes_read = read(fd, buffer, letters);
+    if (bytes_read == -1)
+    {
+        free(buffer);
+        close(fd);
+        return (0);
+    }
+
+    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+
+    free(buffer);
+    close(fd);
+
+    if (bytes_written == -1 || bytes_written != bytes_read)
+        return (0);
+
+    return (bytes_written);
+}
