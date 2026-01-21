ec7f560a268774a83ad98d12c700186407c9d162
Izzat <shixbalayevizzet@example.com>
2026-01-21 11:31:11 +0000

    task0

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+
+/**
+ * hash_table_create - Creates a hash table.
+ * @size: The size of the array.
+ * * Return: A pointer to the newly created hash table,
+ * or NULL if something went wrong.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_table_t *ht;
+    unsigned long int i;
+
+    if (size == 0)
+        return (NULL);
+
+    ht = malloc(sizeof(hash_table_t));
+    if (ht == NULL)
+        return (NULL);
+
+    ht->array = malloc(sizeof(hash_node_t *) * size);
+    if (ht->array == NULL)
+    {
+        free(ht);
+        return (NULL);
+    }
+
+    for (i = 0; i < size; i++)
+        ht->array[i] = NULL;
+
+    ht->size = size;
+
+    return (ht);
+}
