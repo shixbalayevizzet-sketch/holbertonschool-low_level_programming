d4180fa7af69d351be9e2ea8bcec39169b1f59a7
Izzat <shixbalayevizzet@example.com>
2026-01-21 12:01:20 +0000

    task3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,56 +1,55 @@
 #include "hash_tables.h"
 #include <string.h>
+
 /**
  * hash_table_set - Adds an element to the hash table.
  * @ht: The hash table to add or update the key/value to.
  * @key: The key (cannot be an empty string).
- * @value: The value associated with the key (must be duplicated).
+ * @value: The value associated with the key.
  *
  * Return: 1 if it succeeded, 0 otherwise.
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new_node, *temp;
-	char *value_copy;
-	unsigned long int index;
+	char *v_copy;
+	unsigned long int idx;
 
-	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+	if (!ht || !key || !*key || !value)
 		return (0);
 
-	value_copy = strdup(value);
-	if (value_copy == NULL)
+	v_copy = strdup(value);
+	if (!v_copy)
 		return (0);
 
-	index = key_index((const unsigned char *)key, ht->size);
-
-	temp = ht->array[index];
+	idx = key_index((const unsigned char *)key, ht->size);
+	temp = ht->array[idx];
 	while (temp)
 	{
 		if (strcmp(temp->key, key) == 0)
 		{
 			free(temp->value);
-			temp->value = value_copy;
+			temp->value = v_copy;
 			return (1);
 		}
 		temp = temp->next;
 	}
 
 	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
+	if (!new_node)
 	{
-		free(value_copy);
+		free(v_copy);
 		return (0);
 	}
 	new_node->key = strdup(key);
-	if (new_node->key == NULL)
+	if (!new_node->key)
 	{
 		free(new_node);
-		free(value_copy);
+		free(v_copy);
 		return (0);
 	}
-	new_node->value = value_copy;
-	new_node->next = ht->array[index];
-	ht->array[index] = new_node;
-
+	new_node->value = v_copy;
+	new_node->next = ht->array[idx];
+	ht->array[idx] = new_node;
 	return (1);
 }
